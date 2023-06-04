d0e23744a01ac8c438fcca074ec1bcbaf04464b5
Berryverse <134719209+Berryverse@users.noreply.github.com>
2023-06-04 16:30:23 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,29 @@
+
+# While loop, For loop
+
+
+i =1
+
+
+a = list[1, 2, 3, 4, 8, 9, 55]
+
+
+while i < 6:
+    print(i)
+    i += 1
+
+while True:
+    print(i)
+
+
+
+for i in b:
+    if i%2 == 0:
+        print(i)
+        print("It is an even no.")
+    else:
+        print(i)
+        print("It is an odd no.")
+        
+
+
